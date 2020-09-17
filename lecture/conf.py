9b82ee7cc1825d66d1c3bfdc1c55d0dfb8f322c6
José C. García Alanis <12409129+JoseAlanis@users.noreply.github.com>
2020-09-17 15:07:43 +0200

    update authors

--- lecture/conf.py
+++ lecture/conf.py
@@ -18,7 +18,7 @@
 
 project = 'gorella_overview'
 copyright = '2020, G0RELLA'
-author = 'Peer Herholz, Jose Alanis, Christoph Vogelbacher'
+author = 'Peer Herholz, José C. García Alanis, Christoph Vogelbacher'
 
 # -- General configuration ---------------------------------------------------
 
